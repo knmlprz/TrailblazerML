b8cc2a7b00a923687f2981f37af20ce7d368aba3
kubabartecki <169511@stud.prz.edu.pl>
2024-06-26 02:13:43 +0200

    sample usage

--- src/simulation_loop.py
+++ src/simulation_loop.py
@@ -1,6 +1,7 @@
 import numpy as np
 import vision.oak.reading_data as rd
 import map2d.translator as m2d
+from map2d.pcd2track import TrackMaker
 
 if __name__ == "__main__":
     base_path = "./data1500/void_1500-47/stairs0"
@@ -12,6 +13,7 @@ if __name__ == "__main__":
     value_range = (-1, 1)
     # visualization_type - 0 means using open3d, any other number means using regular matplotlib
     mapper = m2d.PointCloudMapper(res, value_range, visualize=True, visualization_type=1)
+    track_maker = TrackMaker(threshold=0.2, has_visualization=True)
 
     # simulation loop
     for image_path, depth_path, pose_path in zip(S3D.images, S3D.sparse_depths, S3D.poses):
@@ -19,4 +21,5 @@ if __name__ == "__main__":
         ### HERE WE CAN ADD SOME FUNCTIONALITY
         # TODO: ALL THE REST
         ###
+        # track = track_maker.point_cloud_to_track(pcd.points)
         map_2d = mapper.point_cloud_to_2d_map(pcd,point)
