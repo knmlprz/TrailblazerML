140de8a0a140d0714fe59cd9b577c54c5981d8be
fwalkowicz <walkowiczf@gmail.com>
2024-07-12 10:40:01 +0200

    add update goal for A*

--- src/main.py
+++ src/main.py
@@ -20,13 +20,14 @@ if __name__ == "__main__":
     point_cloud_mapper = PointCloudMapper(res=5000)
     stm_com = STMCom(port="/dev/ttyUSB0")
     track_maker = TrackMaker()
-    a_star_grid = AStarGrid(point_cloud_mapper.res, point_cloud_mapper.res, start_x=0, start_y=0, end_x=point_cloud_mapper.res - 3, end_y=point_cloud_mapper.res - 2)
+    end_goal = (point_cloud_mapper.res - 3, point_cloud_mapper.res - 2)
+    a_star_grid = AStarGrid(point_cloud_mapper.res, point_cloud_mapper.res, start_x=0, start_y=0, end_x=end_goal[0], end_y=end_goal[1])
     start_loop = True
     while start_loop:
         rgb, pcd, pose = camera_oak.get_data()
         isMarker, markerDict = aruco.read(rgb, False)
         if isMarker:
-            new_destinations = correct_aruco.newDestinations(markerDict, pose)
+            end_goal = correct_aruco.newDestinations(markerDict, pose)
         print(f"pcd {pcd}")
         print(f"pose shape: {pose.shape}, pose: {pose}")
         matrix, first_sector = assignment_to_sectors(pcd)
@@ -36,7 +37,7 @@ if __name__ == "__main__":
         print(f"map_01.shape {map_01.shape}")
         global_map = point_cloud_mapper.cropped_map_to_2d_map(map_01, first_sector)
         print(f"global_map.shape {global_map.shape}")
-        a_star_grid.update(global_map, rover_sector)
+        a_star_grid.update(global_map, rover_sector, end_goal)
         path_to_destination = a_star_grid.a_star_search()
         moves = move(path_to_destination)
         print("move: ", moves, "\n")
