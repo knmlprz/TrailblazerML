38856e50f4b5de195d8b4c1318e102d728641fe4
Kleczyk <mailnasyff@gmail.com>
2024-07-17 17:12:59 +0200

    add reading program

--- src/gps/reading_gps.py
+++ src/gps/reading_gps.py
@@ -9,11 +9,15 @@ ser = serial.Serial(port="/dev/ttyTHS0", baudrate=9600)
 with open("GPSdata.json", 'w') as OutputFile:
     while True:
         newline = ser.readline().decode("utf-8").strip()
-        parsedLine = pynmea2.parse(newline)
-        if parsedLine.find("$GPRMC") > -1:
-            outputDict['latitude'] = parsedLine.latitude
-            outputDict['longitude'] = parsedLine.longitude
-            json.dump(outputDict, OutputFile)
+        try:
+            parsedLine = pynmea2.parse(newline)
+            if parsedLine.find("$GPRMC") > -1:
+                outputDict['latitude'] = parsedLine.latitude
+                outputDict['longitude'] = parsedLine.longitude
+                json.dump(outputDict, OutputFile)
+        except UnicodeDecodeError:
+            print("Invalid byte encountered. Skipping.")
+
 
 
 
