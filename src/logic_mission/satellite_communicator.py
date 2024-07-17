4da893e77664f6b4b80b58027c2ff9fd891adbea
Kleczyk <mailnasyff@gmail.com>
2024-07-17 23:02:17 +0200

    working at stage 1

--- src/logic_mission/satellite_communicator.py
+++ src/logic_mission/satellite_communicator.py
@@ -59,7 +59,7 @@ class SatelliteCommunicator:
         return calculated_checksum == received_checksum
 
     def crc16_ccitt(self, data):
-        crc = 0xFFFF
+        crc = 0x0000
         for byte in data:
             crc ^= byte << 8
             for _ in range(8):
