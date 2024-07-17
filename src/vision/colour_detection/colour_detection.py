4b1d1d5c625141dbcfe0ff3c6671386c1059be8b
fwalkowicz <walkowiczf@gmail.com>
2024-07-17 12:35:38 +0200

    sand colour detection; crop image

--- src/vision/colour_detection/colour_detection.py
+++ src/vision/colour_detection/colour_detection.py
@@ -11,23 +11,25 @@ def detect_red_colour(image: np.ndarray) -> bool:
     """
     # Convert the BGR image to HSV format
     hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    height, width = hsv_image.shape[:2]
+    hsv_image = hsv_image[(width/2) - 10: (width/2) + 10, (height/2) - 10: (height/2) + 10]
 
     # Red colour range
-    red_lower = np.array([136, 87, 111], np.uint8)
-    red_upper = np.array([180, 255, 255], np.uint8)
+    sand_lower = np.array([136, 87, 111], np.uint8)
+    sand_upper = np.array([180, 255, 255], np.uint8)
 
     # Create a mask
-    red_mask = cv2.inRange(hsv_image, red_lower, red_upper)
+    sand_mask = cv2.inRange(hsv_image, sand_lower, sand_upper)
 
     # Dilate the mask to strengthen the red areas
     kernel = np.ones((5, 5), "uint8")
-    red_mask = cv2.dilate(red_mask, kernel)
+    sand_mask = cv2.dilate(sand_mask, kernel)
 
     # Calculate the number of red pixels in the mask
-    red_pixels = cv2.countNonZero(red_mask)
+    sand_pixels = cv2.countNonZero(sand_mask)
 
     # Determine if the color red is detected in the image
-    if red_pixels > 0:
-        return True
+    if sand_pixels > 0:
+        return False
 
-    return False
+    return True
