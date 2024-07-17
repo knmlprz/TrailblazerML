db5681ef00ee545086b399749220676f2e86068c
Kleczyk <mailnasyff@gmail.com>
2024-07-18 01:26:53 +0200

    test on jetson

--- src/logic_mission/satelite_test.py
+++ src/logic_mission/satelite_test.py
@@ -8,25 +8,25 @@ if __name__ == "__main__":
     communicator = SatelliteCommunicator(port="/dev/ttyUSB1", baudrate=115200)
 
     # Send Arm/Disarm command (Assuming 0x01 means Arm)
-    communicator.send_message(SatelliteCommunicator.MSG_ID_ARM_DISARM, b'\x01')
+    communicator.send_message(communicator.MSG_ID_ARM_DISARM, b'\x01')
 
     # Send Navigate GPS command with example coordinates (Latitude, Longitude)
     latitude = 34.052235  # Example latitude
     longitude = -118.243683  # Example longitude
     gps_data = struct.pack('>ff', latitude, longitude)
-    communicator.send_message(SatelliteCommunicator.MSG_ID_NAVIGATE_GPS, gps_data)
+    communicator.send_message(communicator.MSG_ID_NAVIGATE_GPS, gps_data)
 
     # Notify task completion
     communicator.task_completed()
 
     # Set a new stage (Example stage 1)
-    communicator.send_message(SatelliteCommunicator.MSG_ID_SET_STAGE, b'\x01')
+    communicator.send_message(communicator.MSG_ID_SET_STAGE, b'\x01')
 
     # Locate Aruco Tags command
-    communicator.send_message(SatelliteCommunicator.MSG_ID_LOCATE_ARUCO_TAGS, b'')
+    communicator.send_message(communicator.MSG_ID_LOCATE_ARUCO_TAGS, b'')
 
     # Send a detection command (assuming no additional body data is needed)
-    communicator.send_message(SatelliteCommunicator.MSG_ID_DETECTION, b'')
+    communicator.send_message(communicator.MSG_ID_DETECTION, b'')
 
     # Set parameters command (assuming no additional body data is needed)
-    communicator.send_message(SatelliteCommunicator.MSG_ID_SET_PARAMETERS, b'')
+    communicator.send_message(communicator.MSG_ID_SET_PARAMETERS, b'')
