283dae6e282cad313f21d1c69a6becfff70501bd
Kleczyk <mailnasyff@gmail.com>
2024-07-17 10:22:14 +0200

    asd

--- src/logic_mission/logic_mission.py
+++ src/logic_mission/logic_mission.py
@@ -2,8 +2,10 @@ from satellite_communicator import SatelliteCommunicator
 
 class Mission:
     def __init__(self):
-        self.satellite_communicator = SatelliteCommunicator()
-
-    def run(self):
-        self.satellite_communicator.read_message()
+        self.satellite_communicator = SatelliteCommunicator(port="/dev/ttyAMA0", baudrate=9600)
+        self.autonomy_is_on = False
+    def loop_mission(self):
+        while self.autonomy_is_on:
+            self.satellite_communicator.read_message()
+            self
 
