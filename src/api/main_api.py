40421c9e9173a9b0a4e3dd34390d09b536dbc39e
Kleczyk <mailnasyff@gmail.com>
2024-07-19 00:19:16 +0200

    mak

--- src/api/main_api.py
+++ src/api/main_api.py
@@ -15,7 +15,7 @@ class Coordinates(BaseModel):
 @app.get("/v1/position")
 async def get_position():
     try:
-        with open("../utils/position.json", "r") as file:
+        with open("utils/position.json", "r") as file:
             position = json.load(file)
         return position
     except FileNotFoundError:
@@ -25,7 +25,7 @@ async def get_position():
 @app.post("/v1/target")
 async def set_target(target: Coordinates):
     # Użycie absolutnej ścieżki do pliku
-    with open("../utils/target.json", "w") as file:
+    with open("utils/target.json", "w") as file:
         json.dump(target.dict(), file)
     return {"message": "Target saved successfully."}
 
