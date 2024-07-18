a5f32da3b40ec3ea840a9de038261a6e7212af8e
Kleczyk <mailnasyff@gmail.com>
2024-07-19 00:17:28 +0200

    mak

--- src/api/main_api.py
+++ src/api/main_api.py
@@ -3,11 +3,14 @@ import json
 from pydantic import BaseModel
 import uvicorn
 from pathlib import Path
+
 app = FastAPI()
+
 
 class Coordinates(BaseModel):
     latitude: float
     longitude: float
+
 
 @app.get("/v1/position")
 async def get_position():
@@ -18,12 +21,14 @@ async def get_position():
     except FileNotFoundError:
         return {"message": "Position file not found."}
 
+
 @app.post("/v1/target")
 async def set_target(target: Coordinates):
     # Użycie absolutnej ścieżki do pliku
     with open("../utils/target.json", "w") as file:
         json.dump(target.dict(), file)
     return {"message": "Target saved successfully."}
+
 
 def run_api():
     uvicorn.run(app, host="0.0.0.0", port=8899)
