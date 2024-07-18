943e83c3760eacdd68a995bf7328a63328a4f07c
Kleczyk <mailnasyff@gmail.com>
2024-07-19 01:45:26 +0200

    fix path for position.jeson not working in sytemctl

--- src/gps/reading_gps.py
+++ src/gps/reading_gps.py
@@ -1,14 +1,9 @@
 import json
-import os
 import serial
 import pynmea2
 
 outputDict = {}
 ser = serial.Serial(port="/dev/ttyTHS0", baudrate=9600)
-
-# Upewnij się, że katalog istnieje
-output_path = "/home/jetson/repos/TrailblazerML/src/utils"
-os.makedirs(output_path, exist_ok=True)
 
 while True:
     try:
@@ -18,13 +13,9 @@ while True:
             print(f"outputDict: {outputDict}")
             outputDict['latitude'] = parsedLine.latitude
             outputDict['longitude'] = parsedLine.longitude
-            with open(os.path.join(output_path, "position.json"), 'w') as OutputFile:
+            with open("/home/jetson/repos/TrailblazerML/src/utils/position.json", 'w') as OutputFile:
                 json.dump(outputDict, OutputFile)
     except UnicodeDecodeError:
         print("Invalid byte encountered. Skipping.")
     except pynmea2.nmea.ParseError:
         print("Could not parse NMEA sentence. Skipping.")
-    except FileNotFoundError as e:
-        print(f"File not found: {e}")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
