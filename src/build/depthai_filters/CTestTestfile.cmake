31acc3098b6ad95ab9e76357e245d01025f92c27
Konrad Zegar <kondzioklarnet007@gmail.com>
2025-01-29 11:16:41 +0100

    Kamera

---
+++ src/build/depthai_filters/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/konrad/TrailblazerML/src/depthai-ros/depthai_filters
+# Build directory: /home/konrad/TrailblazerML/src/build/depthai_filters
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- src/depth-camera/launch/launch_simulation.launch.py
+++ src/depth-camera/launch/launch_simulation.launch.py
@@ -23,14 +23,29 @@ def generate_launch_description():
         )])
     )
 
+    camera = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('depthai_ros_driver'), 'launch', 'camera_as_part_of_a_robot.launch.py',
+        )])
+    )
+
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=[ '-topic', 'robot_description',
                                    '-entity', 'my_bot'],
                         output='screen')
     
 
+    state_publisher = Node(
+        package='joint_state_publisher_gui', 
+        executable='joint_state_publisher_gui', 
+        name='joint_state_publisher_gui', 
+        output='screen'
+    )
+
     return LaunchDescription([
         rsp,
-        gazebo,
+        #gazebo,
         spawn_entity,
+        camera,
+        state_publisher,
     ])
