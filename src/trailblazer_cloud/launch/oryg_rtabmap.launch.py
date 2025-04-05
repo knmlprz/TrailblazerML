ea8db4669267b4c81db1be762061ea5e4a43ddd2
Rafal Bazan <rafalbe777@gmail.com>
2025-04-05 16:22:26 +0200

    adding oryginal version to launch

--- src/trailblazer_cloud/launch/oryg_rtabmap.launch.py
+++ src/trailblazer_cloud/launch/oryg_rtabmap.launch.py
@@ -1,72 +1,92 @@
-# Requirements:
-#   A OAK-D camera
-#   Install depthai-ros package (https://github.com/luxonis/depthai-ros)
-# Example:
-#   $ ros2 launch rtabmap_examples depthai.launch.py camera_model:=OAK-D
-
 import os
 
 from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
-from launch_ros.actions import Node
-from launch.actions import IncludeLaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+    OpaqueFunction,
+)
+from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import LoadComposableNodes, Node
+from launch_ros.descriptions import ComposableNode
+
+
+def launch_setup(context, *args, **kwargs):
+    name = LaunchConfiguration("name").perform(context)
+    depthai_prefix = get_package_share_directory("depthai_ros_driver")
+
+    params_file = LaunchConfiguration("params_file")
+    parameters = [
+        {
+            "frame_id": name,
+            "subscribe_rgb": True,
+            "subscribe_depth": True,
+            "subscribe_odom_info": True,
+            "approx_sync": True,
+            "Rtabmap/DetectionRate": "3.5",
+        }
+    ]
+
+    remappings = [
+        ("rgb/image", name + "/rgb/image_rect"),
+        ("rgb/camera_info", name + "/rgb/camera_info"),
+        ("depth/image", name + "/stereo/image_raw"),
+    ]
+
+    return [
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(depthai_prefix, "launch", "camera.launch.py")
+            ),
+            launch_arguments={"name": name, "params_file": params_file}.items(),
+        ),
+        LoadComposableNodes(
+            target_container=name + "_container",
+            composable_node_descriptions=[
+                ComposableNode(
+                    package="rtabmap_odom",
+                    plugin="rtabmap_odom::RGBDOdometry",
+                    name="rgbd_odometry",
+                    parameters=parameters,
+                    remappings=remappings,
+                ),
+            ],
+        ),
+        LoadComposableNodes(
+            target_container=name + "_container",
+            composable_node_descriptions=[
+                ComposableNode(
+                    package="rtabmap_slam",
+                    plugin="rtabmap_slam::CoreWrapper",
+                    name="rtabmap",
+                    parameters=parameters,
+                    remappings=remappings,
+                ),
+            ],
+        ),
+        Node(
+            package="rtabmap_viz",
+            executable="rtabmap_viz",
+            output="screen",
+            parameters=parameters,
+            remappings=remappings,
+        ),
+    ]
 
 
 def generate_launch_description():
-    parameters=[{'frame_id':'oak-d-base-frame',
-                 'subscribe_rgbd':True,
-                 'subscribe_odom_info':True,
-                 'approx_sync':False,
-                 'wait_imu_to_init':True}]
-
-    remappings=[('imu', '/imu/data')]
-
-    return LaunchDescription([
-
-        # Launch camera driver
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([os.path.join(
-                get_package_share_directory('depthai_examples'), 'launch'),
-                '/stereo_inertial_node.launch.py']),
-                launch_arguments={'depth_aligned': 'false',
-                                  'enableRviz': 'false',
-                                  'monoResolution': '400p'}.items(),
+    depthai_prefix = get_package_share_directory("depthai_ros_driver")
+    declared_arguments = [
+        DeclareLaunchArgument("name", default_value="oak"),
+        DeclareLaunchArgument(
+            "params_file",
+            default_value=os.path.join(depthai_prefix, "config", "rgbd.yaml"),
         ),
+    ]
 
-        # Sync right/depth/camera_info together
-        Node(   
-            package='rtabmap_sync', executable='rgbd_sync', output='screen',
-            parameters=parameters,
-            remappings=[('rgb/image', '/right/image_rect'),
-                        ('rgb/camera_info', '/right/camera_info'),
-                        ('depth/image', '/stereo/depth')]),
-
-        # Compute quaternion of the IMU
-        Node(
-            package='imu_filter_madgwick', executable='imu_filter_madgwick_node', output='screen',
-            parameters=[{'use_mag': False, 
-                         'world_frame':'enu', 
-                         'publish_tf':False}],
-            remappings=[('imu/data_raw', '/imu')]),
-
-        # Visual odometry
-        Node(
-            package='rtabmap_odom', executable='rgbd_odometry', output='screen',
-            parameters=parameters,
-            remappings=remappings),
-
-        # VSLAM
-        Node(
-            package='rtabmap_slam', executable='rtabmap', output='screen',
-            parameters=parameters,
-            remappings=remappings,
-            arguments=['-d']),
-
-        # Visualization
-        Node(
-            package='rtabmap_viz', executable='rtabmap_viz', output='screen',
-            parameters=parameters,
-            remappings=remappings)
-    ])
+    return LaunchDescription(
+        declared_arguments + [OpaqueFunction(function=launch_setup)]
+    )
