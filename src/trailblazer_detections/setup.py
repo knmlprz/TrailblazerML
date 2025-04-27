c8c806171d94ec949bfbe0e3908499950400f196
piasekj <piasekj1602@gmail.com>
2025-04-27 23:41:00 +0200

    Rosdep fix
    Included necessary packages for dependency management using rosdep
    Cleaned up setup.py

--- src/trailblazer_detections/setup.py
+++ src/trailblazer_detections/setup.py
@@ -1,6 +1,4 @@
 from setuptools import find_packages, setup
-import os
-from glob import glob
 
 package_name = 'trailblazer_detections'
 
@@ -9,12 +7,18 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-        # DODAJEMY MODELE
-        (os.path.join('share', package_name, 'models'), glob('trailblazer_detections/models/*.pt')),
+        ('share/ament_index/resource_index/packages', [
+            'resource/trailblazer_detections'
+        ]),
+        ('share/trailblazer_detections', [
+            'package.xml'
+        ]),
+        ('share/trailblazer_detections/launch', [
+            'launch/yolo_node.launch.py'
+        ]),
+        ('share/trailblazer_detections/models', [
+            'trailblazer_detections/models/urc2024.pt'
+        ]),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
