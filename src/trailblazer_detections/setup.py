4b7e25ce3966f1203043f81c4473ca1d05b15477
piasekj <piasekj1602@gmail.com>
2025-05-03 00:53:41 +0200

    Fix setup.py

--- src/trailblazer_detections/setup.py
+++ src/trailblazer_detections/setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup, find_packages
 from glob import glob
-import os
 
 package_name = 'trailblazer_detections'
 
@@ -12,16 +11,16 @@ setup(
         ('share/ament_index/resource_index/packages', 
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-        (os.path.join('share', package_name, 'models'), glob('models/*')),
+        ('share/' + package_name + '/models', glob('models/*.pt')),
+        ('share/' + package_name + '/launch', glob('launch/*.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='walkowiczf',
     maintainer_email='fillio00@wp.pl',
-    description='YOLO object detection node for ROS2',
+    description='TODO: Package description',
     license='Apache-2.0',
-    #tests_require=['pytest'],
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'yolo_node = trailblazer_detections.yolo_node:main'
