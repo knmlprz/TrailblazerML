beffbd766279fca56eb7b660ed8c88b798b7b5c1
FWalkowicz <fillio00@wp.pl>
2025-05-09 14:45:47 +0200

    launch file

--- src/trailblazer_gps/launch/gps.launch.py
+++ src/trailblazer_gps/launch/gps.launch.py
@@ -1,32 +1,31 @@
+from docutils.nodes import description
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, TextSubstitution
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    port_arg = DeclareLaunchArgument(
-        "port",
-        default_value="/dev/ttyUSB0",
-        description="Port USB, np. /dev/ttyUSB0"
+    gps_type_launch_arg = DeclareLaunchArgument(
+        'gps_type',
+        default_value='rtk_gps',
+        description='Typ GPS: rtk_gps lub basic_gps'
     )
-
-    gps_type_arg = DeclareLaunchArgument(
-        "gps_type",
-        default_value="rtk",
-        description="Typ GPS: 'basic' lub 'rtk'"
+    port_launch_arg = DeclareLaunchArgument(
+        'port',
+        default_value='/dev/ttyUSB0',
+        description='Port szeregowy, do którego podłączony jest GPS'
     )
-
-    gps_type = LaunchConfiguration("gps_type")
-    port = LaunchConfiguration("port")
+    gps_node = Node(
+        package='trailblazer_gps',
+        executable=LaunchConfiguration('gps_type'),
+        name='gps_node',
+        parameters=[{
+            'port': LaunchConfiguration('port'),
+        }]
+    )
 
     return LaunchDescription([
-        port_arg,
-        gps_type_arg,
-
-        Node(
-            package="trailblazer_gps",
-            executable="basic_gps" if gps_type.perform({}) == 'basic' else "gps_rtk",
-            name="gps_publisher",
-            parameters=[{"port": port}]
-        )
+        gps_type_launch_arg,
+        port_launch_arg,
+        gps_node,
     ])
