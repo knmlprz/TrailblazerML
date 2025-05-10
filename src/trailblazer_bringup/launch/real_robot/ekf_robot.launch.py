854c35c4865aee18fd57569b88bbda01e267e6ef
FWalkowicz <fillio00@wp.pl>
2025-05-10 12:15:30 +0200

    working mapviz, gps

---
+++ src/trailblazer_bringup/launch/real_robot/ekf_robot.launch.py
@@ -0,0 +1,82 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    rviz_config_path = os.path.join(
+        get_package_share_directory('trailblazer_rviz'), 'config', 'controller.rviz'
+    )
+
+    rsp = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('trailblazer_description'), 'launch', 'rsp.launch.py'
+        )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
+    )
+
+    controller_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('trailblazer_controller'), 'launch', 'controller.launch.py')
+        )
+    )
+
+    oak_camera_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('trailblazer_oak'), 'launch', 'depthai.launch.py')
+        )
+    )
+
+    # GPS
+    gps_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('trailblazer_gps'), 'launch', 'gps.launch.py')
+        ), 
+        launch_arguments={'port': '/dev/ttyUSB1', 'gps_type': 'rtk_gps'}.items()
+    )
+
+    # EKF
+    gps_wpf_dir = get_package_share_directory(
+        "trailblazer_nav2")
+    rl_params_file = os.path.join(
+        gps_wpf_dir, "config", "dual_ekf_navsat_params.yaml")
+    
+    ekf_launch = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(get_package_share_directory('trailblazer_nav2'), 'launch', 'dual_ekf_navsat.launch.py')
+            )
+        )
+
+    # # # SLAM
+    slam_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('trailblazer_slam'), 'launch', 'slam.launch.py')
+        )
+    ),
+
+    # NAV2
+    nav2_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('trailblazer_nav2'), 'launch', 'navigation_launch.py')
+        ),
+        launch_arguments={'use_sim_time': 'true', 'params_file': nav2_config_path, 'autostart': 'true'}.items()
+    ),
+
+    rviz_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('trailblazer_rviz'), 'launch', 'rviz.launch.py')
+        ),
+        launch_arguments={'rviz_config': rviz_config_path}.items()
+    )
+
+    return LaunchDescription([
+        rsp,
+        controller_launch,
+        oak_camera_launch,
+        gps_launch,
+        ekf_launch,
+        rviz_launch
+    ])
