0ea061b8629e135f3585cd4ab6f050206c40fb64
Rafal Bazan <rafalbe777@gmail.com>
2025-05-16 18:04:07 +0200

    testing navsat positive

--- src/trailblazer_bringup/launch/real_robot/camera_robot.launch.py
+++ src/trailblazer_bringup/launch/real_robot/camera_robot.launch.py
@@ -1,22 +1,31 @@
 from ament_index_python.packages import get_package_share_directory
+
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, OpaqueFunction
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
+import os
+
 def launch_setup(context, *args, **kwargs):
+
     # Directories
-    pkg_nav2_bringup = get_package_share_directory('nav2_bringup')
-    pkg_trailblazer_cloud = get_package_share_directory('trailblazer_cloud')
+    pkg_nav2_bringup = get_package_share_directory(
+        'nav2_bringup')
+    pkg_trailblazer_cloud = get_package_share_directory(
+        'trailblazer_cloud')
     
     nav2_params_file = PathJoinSubstitution(
         [FindPackageShare('trailblazer_cloud'), 'params', 'trailblazer_rgbd_nav2_params.yaml']
     )
+    gps_params_file = PathJoinSubstitution(
+        [FindPackageShare('trailblazer_nav2'), 'config', 'dual_ekf_navsat_params.yaml']
+    )
 
     pkg_trailblazer_description = get_package_share_directory('trailblazer_description')
     pkg_trailblazer_controller = get_package_share_directory('trailblazer_controller')
+    pkg_trailblazer_nav2 = get_package_share_directory('trailblazer_nav2')
 
     # Paths
     nav2_launch = PathJoinSubstitution(
@@ -29,12 +38,9 @@ def launch_setup(context, *args, **kwargs):
         [pkg_trailblazer_description, 'launch', 'rsp.launch.py'])
     controller_launch_path = PathJoinSubstitution(
         [pkg_trailblazer_controller, 'launch', 'controller.launch.py'])
+    gps_launch_path = PathJoinSubstitution(
+        [pkg_trailblazer_nav2, 'launch', 'dual_ekf_navsat.launch.py'])
     
-    # Robot localization parameters
-    rl_params_file = PathJoinSubstitution(
-        [FindPackageShare('trailblazer_cloud'), 'params', 'robot_localization_params.yaml']
-    )
-
     # Includes
     nav2 = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([nav2_launch]),
@@ -59,28 +65,14 @@ def launch_setup(context, *args, **kwargs):
         launch_arguments=[
             ('use_sim_time', 'false'),
             ('use_ros2_control', 'true')
-        ]
-    )
-    controller_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([controller_launch_path])
-    )
-    
-    # Navsat Transform Node
-    navsat_transform = Node(
-        package="robot_localization",
-        executable="navsat_transform_node",
-        name="navsat_transform",
-        output="screen",
-        parameters=[rl_params_file, {"use_sim_time": False}],
-        remappings=[
-            ("imu/data", "imu/data"),
-            ("gps/fix", "gps/fix"),
-            ("gps/filtered", "gps/filtered"),
-            ("odometry/gps", "odometry/gps"),
-            ("odometry/filtered", "odometry/global"),
-        ],
-    )
+        ])
 
+    controller_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([controller_launch_path]))
+    
+    gps_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([gps_launch_path]))
+    
     return [
         # Nodes to launch
         nav2,
@@ -88,11 +80,12 @@ def launch_setup(context, *args, **kwargs):
         rtabmap,
         rsp,
         controller_launch,
-        #navsat_transform  # Dodano węzeł navsat_transform
+        #gps_launch,
     ]
 
 def generate_launch_description():
     return LaunchDescription([
+        
         # Launch arguments
         DeclareLaunchArgument(
             'localization', default_value='false',
