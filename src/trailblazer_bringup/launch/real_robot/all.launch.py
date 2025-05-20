ccb78853d3726dcc8a92dbf411382ba13ae39dd5
Rafal Bazan <rafalbe777@gmail.com>
2025-05-20 22:32:07 +0200

    reseting old files

--- src/trailblazer_bringup/launch/real_robot/all.launch.py
+++ src/trailblazer_bringup/launch/real_robot/all.launch.py
@@ -27,8 +27,11 @@ def launch_setup(context, *args, **kwargs):
     # nav2_params_file = PathJoinSubstitution(
     #     [FindPackageShare('trailblazer_cloud'), 'params', 'trailblazer_rgbd_nav2_params.yaml']
     # )
+    # nav2_params_file = PathJoinSubstitution(
+    #     [FindPackageShare('trailblazer_nav2'), 'config', 'nav2_params.yaml']
+    # )
     nav2_params_file = PathJoinSubstitution(
-        [FindPackageShare('trailblazer_nav2'), 'config', 'nav2_params.yaml']
+        [FindPackageShare('trailblazer_nav2'), 'config', 'rgbd_nav2_params.yaml']
     )
     nav2 = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([nav2_launch]),
