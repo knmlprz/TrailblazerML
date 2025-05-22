b976f06c587c42b633985fc171899b8aefe38ad4
Damian Kobyliński <damiankob12@gmail.com>
2025-05-22 16:40:37 +0200

    feat!: changes with Docker README in Polish

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,28 +4,20 @@ set -e
 
 cd /TrailblazerML
 
-# ponizsze komendy są zakomentowane bo rosdep i colcon na ten moment musza 
-# byc bezposrednio w terminalu uruchomione nie w tym entrypoint
-
-#rosdep install --from-paths src --ignore-src -r -y
-
 SETUP_BASH="install/setup.bash"
 
 if [ ! -d "install" ]; then
     echo "Workspace not built. Building with colcon"
-    #colcon build
+    colcon build
 else
     echo "Workspace already built"
 fi
 
 if [ -f "$SETUP_BASH" ]; then
-    # shellcheck disable=SC1090
-    #source install/setup.bash
-    echo "tak"
+    source install/setup.bash
 else
     echo "$SETUP_BASH does not exist. Please build your workspace first."
     exit 1
 fi
 cd /TrailblazerML
-terminator
 exec "$@"
